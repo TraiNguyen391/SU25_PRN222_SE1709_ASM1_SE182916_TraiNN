7872e010fab57a5b37c7fe5caba0557c8f96077e
Resonance <ngoctrainguyen203@gmail.com>
2025-07-05 13:40:31 +0700

    update phân quyền delete

--- SchoolMedical.RazorWebApp.TraiNN/Pages/MedicationTraiNNs/Delete.cshtml.cs
+++ SchoolMedical.RazorWebApp.TraiNN/Pages/MedicationTraiNNs/Delete.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ using SchoolMedical.Service.TraiNN;
 
 namespace SchoolMedical.RazorWebApp.TraiNN.Pages.MedicationTraiNns
 {
+    [Authorize(Roles = "1")]
     public class DeleteModel : PageModel
     {
         private readonly IMedicationTraiNNService _medicationTraiNNService;
